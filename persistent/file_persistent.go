51b592b58178537981f283ee6fee2876ef0647d1
sunfeilong <sunfeilong1993@gmail.com>
2020-06-03 14:52:04 +0800

    删除 errors 包

--- persistent/file_persistent.go
+++ persistent/file_persistent.go
@@ -1,11 +1,11 @@
 package persistent
 
 import (
-    "../errors"
     "../model"
     "../s-logger"
     "../tool"
     "encoding/json"
+    "errors"
     "io/ioutil"
     "os"
 )
@@ -23,17 +23,17 @@ func (c CompanyFilePreserver) Save(data []model.Company) error {
     path, err := tool.GetPath(pathName, maxLevel, maxLevel)
     if nil != err {
         logger.Infow("保存数据到文件,未找到配置路径", "pathName", pathName, "err", err)
-        return errors.StockDataError{Msg: "保存数据到文件,未找到配置路径"}
+        return errors.New("保存数据到文件,未找到配置路径")
     }
     marshal, err := json.MarshalIndent(data, "", "  ")
     if nil != err {
         logger.Infow("保存数据到文件,数据格式化异常", "pathName", pathName, "err", err)
-        return errors.StockDataError{Msg: "保存数据到文件,数据格式化异常"}
+        return errors.New("保存数据到文件,数据格式化异常")
     }
     err = ioutil.WriteFile(path+c.getFullFileName(tool.NowDate()), marshal, os.ModeAppend)
     if nil != err {
         logger.Infow("保存数据到文件,写入文件数据异常", "pathName", pathName, "err", err)
-        return errors.StockDataError{Msg: "保存数据到文件,写入文件数据异常"}
+        return errors.New("保存数据到文件,写入文件数据异常")
     }
     return nil
 }
@@ -42,19 +42,19 @@ func (c CompanyFilePreserver) Read() ([]model.Company, error) {
     path, err := tool.GetPath(pathName, maxLevel, maxLevel)
     if nil != err {
         logger.Infow("从文件读取数据,未找到配置路径", "pathName", pathName, "err", err)
-        return nil, errors.StockDataError{Msg: "从文件读取数据,未找到配置路径"}
+        return nil, errors.New("从文件读取数据,未找到配置路径")
     }
     file, err := ioutil.ReadFile(path + c.getFullFileName(tool.NowDate()))
     if nil != err {
         logger.Infow("从文件读取数据,读取数据异常", "pathName", pathName, "err", err)
-        return nil, errors.StockDataError{Msg: "从文件读取数据,读取数据异常"}
+        return nil, errors.New("从文件读取数据,读取数据异常")
     }
 
     d := &[]model.Company{}
     err = json.Unmarshal(file, &d)
     if nil != err {
         logger.Infow("从文件读取数据,解析数据异常", "pathName", pathName, "err", err)
-        return nil, errors.StockDataError{Msg: "从文件读取数据,读取数据异常"}
+        return nil, errors.New("从文件读取数据,读取数据异常")
     }
     return *d, nil
 }
