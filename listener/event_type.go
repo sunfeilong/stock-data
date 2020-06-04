bdc9b42f74e3b9f4c900083ac656d0751973fea5
sunfeilong <sunfeilong1993@gmail.com>
2020-06-04 11:19:39 +0800

    删除事件模块

--- listener/event_type.go
+++
@@ -1,5 +0,0 @@
-package listener
-
-const (
-    CollectCompanyInfoFinished string = "CollectCompanyInfoFinished"
-)
--- listener/listener.go
+++
@@ -1,9 +0,0 @@
-package listener
-
-//事件监听器
-type Listener interface {
-    GetId() string
-    GetEventType() string
-    String() string
-    Notify()
-}
