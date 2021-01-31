4bed909f1c57c11c89e487d6f59249ccd2ba0f07
sunfeilong <sunfeilong1993@gmail.com>
2021-01-31 12:14:22 +0800

    token 获取方法

--- pkg/collector/token/hk_token_test.go
+++ pkg/collector/token/hk_token_test.go
@@ -6,6 +6,6 @@ import (
 )
 
 func TestGetHKToken(t *testing.T) {
-    token := GetHKToken("https://www.hkex.com.hk/?sc_lang=zh-HK")
+    token := GetHKToken("https://www.hkex.com.hk/Market-Data/Securities-Prices/Equities?sc_lang=zh-HK")
     assert.NotEmpty(t, token, "GET TOKEN FAILED")
 }
